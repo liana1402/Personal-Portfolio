3ca5b545173af3ae8fb60d9919aff258b7723b7e
Liana <145667748+liana1402@users.noreply.github.com>
2024-08-12 00:11:24 +0100

    finished experience page, finished project mobile, first version of website done

--- js/swap-switch.js
+++ js/swap-switch.js
@@ -1,12 +1,18 @@
 const switchSlider = document.getElementById("switch");
 const journey = document.getElementById("journey-page");
+const journeyFooter = document.getElementById("journey-footer");
 const experience = document.getElementById("experience-page");
+const experienceFooter = document.getElementById("experience-footer");
 switchSlider.addEventListener('click', function(){
     if(switchSlider.checked){
         journey.style.display = "none";
+        journeyFooter.style.display = "none";
         experience.style.display = "flex";
+        experienceFooter.style.display = "block";
     }else{
         journey.style.display = "flex";
+        journeyFooter.style.display = "block"
         experience.style.display = "none";
+        experienceFooter.style.display = "none";
     }
 });
